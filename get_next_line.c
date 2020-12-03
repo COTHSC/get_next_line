c626538effa846d7b45151e5a97d157b781009fe
jean <thierryscully.colomban@gmail.com>
2020-12-03 19:21:27 +0100

    work in progress

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,24 @@
 /*   By: jean <jescully@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/27 16:36:29 by jean              #+#    #+#             */
-/*   Updated: 2020/12/03 19:03:20 by jean             ###   ########.fr       */
+/*   Updated: 2020/12/03 19:19:55 by jean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h> 
 #include "get_next_line.h"
-char	*cleaned_up_line(char *line)
+
+static char	*cleaned_up_line(char *line)
 {
-	int i;
-	char *clean_line;
+	int			i;
+	char		*clean_line;
 
-	i = 0;	
+	i = 0;
 	if (!line || ft_strlen(line) == 0)
-	{	 
-		 clean_line = (char*)malloc(1);
-		 clean_line[0] = '\0';
-		 return (clean_line);
+	{
+		clean_line = (char*)malloc(1);
+		clean_line[0] = '\0';
+		return (clean_line);
 	}
-
 	while (line[i] && line[i] != '\n')
 		i++;
 	if (!(clean_line = (char*)(malloc(sizeof(char) * (i + 1)))))
@@ -39,17 +38,17 @@ static char	*leftovers(char *available_string)
 
 	if (!available_string || ft_strlen(available_string) == 0)
 		return (NULL);
-	if (!ft_strchr(available_string, '\n')) 
+	if (!ft_strchr(available_string, '\n'))
 	{
-			free(available_string);
-			return NULL;
+		free(available_string);
+		return (NULL);
 	}
 	new_string = ft_strdup(ft_strchr(available_string, '\n') + 1);
 	free(available_string);
 	return (new_string);
 }
 
-int	check_the_input(char *bufstr, int const fd, char **line)
+static int	check_the_input(char *bufstr, int const fd, char **line)
 {
 	if (fd < 0 || fd > 1024 || !bufstr || !line)
 	{
@@ -60,15 +59,15 @@ int	check_the_input(char *bufstr, int const fd, char **line)
 		return (1);
 }
 
-int	get_next_line(int const fd, char **line)
+int			get_next_line(int const fd, char **line)
 {
 	static char *available_string;
-	char bufstr[BUFFER_SIZE + 1];
-	int bytes;
-	
+	char		bufstr[BUFFER_SIZE + 1];
+	int			bytes;
+
 	bytes = 1;
 	if (!check_the_input(bufstr, fd, line))
-		return -1;
+		return (-1);
 	if (BUFFER_SIZE <= 0)
 		return (-1);
 	while (!ft_strchr(available_string, '\n') && bytes != 0)
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jescully <jescully@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/16 13:43:14 by jescully          #+#    #+#             */
-/*   Updated: 2020/12/03 18:50:10 by jean             ###   ########.fr       */
+/*   Updated: 2020/12/03 19:21:07 by jean             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,11 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-int			get_next_line(int const fd, char **line);
+int				get_next_line(int const fd, char **line);
 char			*ft_strchr(const char *s, int c);
 size_t			ft_strlcpy(char *dst, const char *src, size_t size);
 size_t			ft_strlen(const char *s);
 char			*ft_strdup(const char *s);
-char			*ft_strjoin(char  *s1, char *s2);
-void    *ft_memmove(void *dest, const void *src, size_t n);
+char			*ft_strjoin(char *s1, char *s2);
+void			*ft_memmove(void *dest, const void *src, size_t n);
 #endif
-
-
